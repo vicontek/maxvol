24cc4c0d6cb3b78b8b82d7b8a8022ead4728eb55
vicontek <vicontek@gmail.com>
2018-12-18 13:16:26 +0300

    Initial

---
+++ datasets/get_mnist.sh
@@ -0,0 +1,8 @@
+mkdir -p mnist
+cd mnist
+wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz  && gzip --decompress train-images-idx3-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz  && gzip --decompress train-labels-idx1-ubyte.gz
+
+wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz && gzip --decompress t10k-images-idx3-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz && gzip --decompress t10k-labels-idx1-ubyte.gz 
+
